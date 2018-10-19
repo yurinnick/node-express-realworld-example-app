ec0ae310e08ef1f7762c00a74eb1e8e7025acdd2
Nikolay Yurin <nyurin@griddynamics.com>
2018-10-18 21:07:35 -0700

    Add status api

--- routes/api/health.js
+++ routes/api/health.js
@@ -1,7 +1,16 @@
 var router = require('express').Router();
+var mongoose = require('mongoose');
 
-router.get('/', function(req, res, next) {
+router.get('/health', function(req, res, next) {
   res.json({status: 'UP'});
+});
+
+router.get('/status', function(req, res, next) {
+  dbStatus = !!mongoose.connection.readyState ? 'Running' : 'Connection failed'
+  res.json({
+    api: 'Running',
+    database: dbStatus
+  });
 });
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 var router = require('express').Router();
 
 router.use('/', require('./users'));
+router.use('/', require('./health'));
 router.use('/profiles', require('./profiles'));
 router.use('/articles', require('./articles'));
 router.use('/tags', require('./tags'));
-router.use('/health', require('./health'));
 
 router.use(function(err, req, res, next){
   if(err.name === 'ValidationError'){
